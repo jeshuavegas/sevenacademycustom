5c5335dc492ced56843ea9f0d91293693df42606
Mr. Intangble <mrintangble@gmail.com>
2024-09-27 10:30:15 -0400

    shortcode year update

--- sevenacademycustom.php
+++ sevenacademycustom.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              https://mysevenacademy.com
- * @since             1.0.4
+ * @since             1.0.0
  * @package           Sevenacademycustom
  *
  * @wordpress-plugin
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SEVENACADEMYCUSTOM_VERSION', '1.0.4' );
+define( 'SEVENACADEMYCUSTOM_VERSION', '1.0.5' );
 
 /**
  * The code that runs during plugin activation.
